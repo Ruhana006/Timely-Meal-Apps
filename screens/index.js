532c62fcc4db21e8aecdd1d3ff925a7570198ed3
S.m.Samrat <imsamrat24@gmail.com>
2021-06-04 18:41:51 +0600

    Google Map added

--- screens/index.js
+++ screens/index.js
@@ -1,9 +1,11 @@
 import Home from './Home';
 import Restaurant from './Restaurant';
 import OrderDelivery from './OrderDelivery';
+import Map from './Map';
 
 export {
     Home,
     Restaurant,
-    OrderDelivery
+    OrderDelivery,
+    Map
 }
