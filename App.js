886836737515d754995aff028a0a203d7f6f26f2
iftekharemon703 <iftekharemon703@gmail.com>
2021-06-04 07:41:57 +0600

    complete navigation

--- App.js
+++ App.js
@@ -1,27 +1,28 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 
-import Tabs from './navigation/tabs';
-import {Home, Restaurant, OrderDelivery} from './screens';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native'
+
+import { Restaurant, OrderDelivery } from './screens'
+import Tabs from './navigation/tabs'
 
 const Stack = createStackNavigator();
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          HeaderShown: false
-        }}
-        initialRouteName={"Home"}
-      >
-        <Stack.Screen name="Home" component={Tabs} />
-        <Stack.Screen name="Restaurant" component={Restaurant} />
-        <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false
+                }}
+                initialRouteName={'Home'}
+            >
+                <Stack.Screen name="Home" component={Tabs} />
+                <Stack.Screen name="Restaurant" component={Restaurant} />
+                <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
 
 export default App;
